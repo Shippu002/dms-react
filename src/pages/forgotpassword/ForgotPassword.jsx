01bf2dc143cf11f3c7542a51005c31a739b9a5a6
Shippu002 <adelajaalameen@gmail.com>
2024-11-12 05:44:10 -0800

    updating

--- src/pages/forgotpassword/ForgotPassword.jsx
+++ src/pages/forgotpassword/ForgotPassword.jsx
@@ -1,11 +1,63 @@
 import { Link } from "react-router-dom";
+import styles from "./forgotpassword.modules.css"
+import { useState } from "react";
+import Button from "../../components/button/Button";
+import Logo from "../../images/Logo.svg";
+
 
 const ForgotPassword = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email,);
+  };
+
   return (
     <div>
-      <h1>ForgotPassword</h1>
-      <Link to="/login">Login</Link>
+      <img src={Logo} alt="dms logo" className={styles.logo} />
+      <section classname={styles.forgotpasswordcontainer}>
+        <section classname={styles.headingcontainer}>
+          <h4>Forgot Password</h4>
+          <p>Please enter your email address</p>
+        </section>
+        <form onSubmit={handleSubmit} className={styles.formwrapper}>
+          <div>
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              placeholder="Enter name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </form>
+
+        <div className="styles.butttonwrapper">
+          <Button url="/login" text="Submit" />
+
+          <Button url="/login" text="Confirm" />
+        </div>
+      </section>
     </div>
   );
+
 };
+
+  
+
 export default ForgotPassword;
